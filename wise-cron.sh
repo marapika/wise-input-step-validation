5627e777495717df1f21f2f693ccb1e307fa711f
marapika <79537661+marapika@users.noreply.github.com>
2021-08-16 09:48:26 +0200

    Update wise-cron.sh

--- wise-cron.sh
+++ wise-cron.sh
@@ -14,12 +14,12 @@ then
 #checking if the latest file is newer than the previous version of properties
 elif [ ${folder}/application.properties -nt ${folder}/previous_application.properties ]
 then
-	#concatenating previous and cuurrent version to capture changes. Stored in a unique file with check timestam
+	#concatenating previous and cuurrent version to capture changes. Stored in a unique file with check timestamp
 	cat ${folder}/previous_application.properties  > ${folder}/CHANGES_application.properties.${timestamp}
 	echo "Previous user data replaced by: " >> ${folder}/CHANGES_application.properties.${timestamp}
 	cat ${folder}/application.properties >> ${folder}/CHANGES_application.properties.${timestamp}
 
-	#updating current version of file for the purpose of future changes check
+	#updating current version of the file for the purpose of future changes checking
 	cp ${folder}/application.properties ${folder}/previous_application.properties
 
 fi
